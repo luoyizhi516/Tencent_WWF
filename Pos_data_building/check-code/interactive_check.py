e28ac3a2242607cbd7c7e72b7a03b3db1dcaafbb
JingyuanChen1423 <jingyuanchen1423@gmail.com>
2021-06-29 15:11:32 +0800

    update

--- Pos_data_building/check-code/interactive_check.py
+++ Pos_data_building/check-code/interactive_check.py
@@ -5,10 +5,10 @@ import cv2,argparse
 def main():
     base,txt_path,start,end=opt.img_dir,opt.text_dir,opt.start,opt.end
     img_list=os.listdir(base)
-    txt_path=r'D:/WWF/data-check-list\check-list-part2/check0-33.txt'
+    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part3/check0-10.txt'
     with open(txt_path, 'w') as f:
 
-        for i in img_list[0:33]:
+        for i in img_list[0:10]:
             img_dir=base+i
             print(img_dir)
     
@@ -31,7 +31,7 @@ def main():
                 break
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img_dir', type=str, default='D:/top14-sup-transformed/visualization/', help='dataset_store_dir')
+    parser.add_argument('--img_dir', type=str, default=r'D:\WWF_Det\WWF_Data\Pos_Data\top14-part3\allset\visualizations/', help='dataset_store_dir')
     parser.add_argument('--text_dir', type=str, default='check.txt', help='result')
     parser.add_argument('--start', type=int, default='0', help='start_img')
     parser.add_argument('--end', type=int, default='3210', help='start_img')
