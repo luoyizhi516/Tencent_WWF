1a9da2070de4c7619ddc7fee9224e2d11d6452c2
JingyuanChen1423 <jingyuanchen1423@gmail.com>
2021-06-29 20:54:42 +0800

    update

---
+++ Final_data_building/video_based_split/modality/imageio-top14-sup.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+import os
+import pandas as pd
+import shutil
+import numpy as np
+import cv2
+from tqdm import tqdm
+import pyfastcopy
+import json
+import imageio,skimage
+from xml.etree import ElementTree
+from PIL import Image, ImageDraw, ImageFont
+
+def imageio_stat(video_path):
+    vid = imageio.get_reader(video_path,  'ffmpeg')
+    first=True
+    for i in vid:
+        im=i
+        first=False
+        if not first:
+            break
+    data = skimage.img_as_float(im)
+    data = data / data.max() #normalizes data in range 0 - 255
+    data = 255 * data
+    frame = data.astype(np.uint8)
+    b, g, r = cv2.split(frame)
+    if np.mean(b-g)<5:
+        modality="Infra"
+    else: modality="RGB"
+    return modality
+def main():
+    dataset_name='top14-all/'
+    save_base='D:/WWF_Det/WWF_Det/Raw_data_stat/'+dataset_name
+    df_path=save_base+'top14-all-modality.csv'
+    df=pd.read_csv(df_path)
+    null_df=df[df.isnull().T.any()]
+    for index, row in null_df.iterrows():
+        video_path=row['video_path']
+        try:
+            modality=imageio_stat(video_path)
+        except:
+            print(video_path)
+            continue
+        else:
+            df.loc[index,'modality']=modality
+
+    
+if __name__ == "__main__":
+    main()
---
+++ Final_data_building/video_based_split/modality/top14-all-imageio.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+import os
+import pandas as pd
+import shutil
+import numpy as np
+import cv2
+from tqdm import tqdm
+import pyfastcopy
+import json
+import imageio
+import skimage
+from xml.etree import ElementTree
+from PIL import Image, ImageDraw, ImageFont
+def imageio_stat(video_path):
+    vid = imageio.get_reader(video_path,  'ffmpeg')
+    first=True
+    for i in vid:
+        im=i
+        first=False
+        if not first:
+            break
+    data = skimage.img_as_float(im)
+    data = data / data.max() #normalizes data in range 0 - 255
+    data = 255 * data
+    frame = data.astype(np.uint8)
+    b, g, r = cv2.split(frame)
+    if np.mean(b-g)<5:
+        modality="Infra"
+    else: modality="RGB"
+    return modality,frame
+def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
+    if (isinstance(img, np.ndarray)):
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    fontText = ImageFont.truetype('simsun.ttc', textSize, encoding="utf-8")
+    draw.text((left, top), text, textColor, font=fontText)
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+
+def main():
+    base='D:/WWF_Det/WWF_Data/Raw_Data/top14-all/'
+    
+    for cate in tqdm(os.listdir(base)):
+        cate_base=base+cate+'/videos/'
+        modality_base=base+cate+'/modality/'
+        if not os.path.exists(modality_base): os.makedirs(modality_base)
+        for vid_name in tqdm(os.listdir(cate_base)):
+            vid_id=cate_base+vid_name
+            modality,frame=imageio_stat(vid_id)
+            frame=cv2ImgAddText(frame, modality , 100,100,(255,0,0),50)
+
+            cv2.imwrite(modality_base+vid_name[:-4]+'.jpg',frame)
+if __name__ == "__main__":
+    main()
