5071c89798eb6f16c0fed7b72afa5c2f51505fb2
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-07 18:43:45 +0800

    update

--- Final_data_building/video_based_split/split_vid_top14_p123.py
+++ Final_data_building/video_based_split/split_vid_top14_p123.py
@@ -26,7 +26,7 @@ def main():
         df_cri=pd.read_csv(critiria_path)
         cate_list=np.unique(df['cate'].values)
         np.random.seed(2021)
-        #print(df)
+        
         for cate in cate_list:
             
             infra_index=df.loc[(df['cate'] == cate)&(df['modality'] == 'Infra')].sample(frac=1).index.values.tolist()
@@ -41,12 +41,19 @@ def main():
             train_index_all=[i for i in infra_index+rgb_index if i not in test_index_all]
             
             for ID in test_index_all:
+            
                 df.loc[ID,'label']='test'
+                ori_dir=df.loc[ID,'video_path']
+                cate=df.loc[ID,'cate']
+                target_base=os.path.join('D:/WWF_Det/WWF_Data/Final_Data/valset-vid-v1/',cate)
+                target_dir=os.path.join(target_base,os.path.splitext(ori_dir)[1])
+                if not os.path.exists(target_base): os.makedirs(target_base)
+                shutil.copyfile(ori_dir,target_dir)
             for ID in train_index_all:
                 df.loc[ID,'label']='train'
             #print(df)
             #break
-        df.to_csv(save_path,index=False)
+        # df.to_csv(save_path,index=False)
 
 if __name__ == "__main__":
     
