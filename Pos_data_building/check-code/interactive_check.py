898ef5517939f0a42fcea8d9bb21891710a57f08
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-21 03:06:56 +0800

    update codes

--- Pos_data_building/check-code/interactive_check.py
+++ Pos_data_building/check-code/interactive_check.py
@@ -6,10 +6,10 @@ def main():
     base,txt_path,start,end=opt.img_dir,opt.text_dir,opt.start,opt.end
     img_list=os.listdir(base)
     
-    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part6/check4100-4300.txt'
+    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part6/check4500-all.txt'
     with open(txt_path, 'w') as f:
 
-        for i in img_list[4100:4300]:
+        for i in img_list[4500:]:
             img_dir=base+i
             print(img_dir)
     
