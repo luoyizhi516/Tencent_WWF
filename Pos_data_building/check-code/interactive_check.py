2702f18ced40d93397d252b8bbc4dc7d24ca030d
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-21 02:21:52 +0800

    update results

--- Pos_data_building/check-code/interactive_check.py
+++ Pos_data_building/check-code/interactive_check.py
@@ -6,10 +6,10 @@ def main():
     base,txt_path,start,end=opt.img_dir,opt.text_dir,opt.start,opt.end
     img_list=os.listdir(base)
     
-    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part5/check2300-2500.txt'
+    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part5/check2500-2700.txt'
     with open(txt_path, 'w') as f:
 
-        for i in img_list[2300:2500]:
+        for i in img_list[2500:2700]:
             img_dir=base+i
             print(img_dir)
     
