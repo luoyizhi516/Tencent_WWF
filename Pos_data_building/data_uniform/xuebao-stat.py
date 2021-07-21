761103f05096493da51c2480bb14b3b4b27cc6c9
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-21 20:30:02 +0800

    add files

--- Pos_data_building/data_uniform/xuebao-stat.py
+++ Pos_data_building/data_uniform/xuebao-stat.py
@@ -16,9 +16,9 @@ from sklearn.model_selection import train_test_split
 
 def main():
     
-    dataset_name='top14-part6'
-    check_txt=r"D:/WWF_Det\WWF_Det\Drop_txt/top14-p6-all.txt"
-    stat_csv='D:/WWF_Det/WWF_Det/Pos_data_stat/top14-part6-dataset-stat.csv'
+    dataset_name='xuebao-120-all'
+    check_txt=r"D:/WWF_Det\WWF_Det\Drop_txt/xuebao-120-all.txt"
+    stat_csv='D:/WWF_Det/WWF_Det/Pos_data_stat/xuebao-dataset-stat.csv'
 
     #"D:\WWF\data-check-list\check_list\check-all.txt"
 
