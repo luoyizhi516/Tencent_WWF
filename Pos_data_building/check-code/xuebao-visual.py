ef1fd258b4e578dce9f169947e7ffdafd6c6badd
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-22 12:38:39 +0800

    update

--- Pos_data_building/check-code/xuebao-visual.py
+++ Pos_data_building/check-code/xuebao-visual.py
@@ -27,46 +27,51 @@ def main():
     data_set='D:/WWF_Det/WWF_Data/Raw_Data/'
     box_num=0
     cate_class=['baichunlu','chihu','gaoyuanshanchun','gaoyuantu','lanmaji','ma','malu','maoniu','mashe','person','xuebao','yang','yanyang','zanghu','chai','hanta','huangmomao','lang','lv','pao','sheli','shidiao','zongxiong']
-    for index, row in tqdm(df.iterrows()):
-        timu_data=json.loads(row['题目数据'])
-        pic_id=row['题目ID']
-        file_path=data_set+timu_data['Path']
-        
-        visual_folder='D:/WWF_Det/WWF_Data/Pos_Data/xuebao-120-all-improved/allset/visualizations/'
+    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\xuebao-120-all/extra.txt'
 
-        if not os.path.exists(visual_folder): 
-            os.makedirs(visual_folder, exist_ok = True)
-        file_path=file_path.replace('/top14-part6-raw/','/top14-p6/',1)
-        assert os.path.exists(file_path),file_path
-        
-        cate=timu_data['Path'].split('/')[1]
-        image_name=str(pic_id)+'.jpg'
-        label_dict=json.loads(row['标注答案'])
-        if len(label_dict.keys()):
-            bboxes=label_dict['objects']
-            img = cv2.imread(file_path)
-            imgy,imgx=img.shape[:2]
+    with open(txt_path, 'w') as f:
+        for index, row in tqdm(df.iterrows()):
+            timu_data=json.loads(row['题目数据'])
+            pic_id=row['题目ID']
+            file_path=data_set+timu_data['Path']
             
-            if len(bboxes):
-                for bbox in bboxes:
-                    box_num+=1
-                    x_list=[bbox['data'][i]['x'] for i in range(0,4)]
-                    y_list=[bbox['data'][i]['y'] for i in range(0,4)]
-                    topleft=[int(max(min(x_list),0)),int(max(min(y_list),0))]
-                    bottomright=[int(min(max(x_list),imgx-1)),int(min(max(y_list),imgy-1))]
-                    cv2.rectangle(img, tuple(topleft), tuple(bottomright),(0,255,0), 2, 4)
-                    center_x=int((topleft[0]+bottomright[0])/2)
-                    center_y=int((topleft[1]+bottomright[1])/2)
-                    if len(bbox['tags']):
-                        if 'value'in bbox['tags'][0].keys():
-                            value=bbox['tags'][0]['value']+[cate]
-                            class_name=str(value)
+            visual_folder='D:/WWF_Det/WWF_Data/Pos_Data/xuebao-120-all-improved/allset/visualizations/'
 
-                            img=cv2ImgAddText(img, class_name , int(topleft[0]),center_y,(0,255,255),20)
-                        #img=cv2ImgAddText(img,str(pic_id),10,10,(0,0,0),30)
-        #ONLY plot the image if it is labeled.
-            if not os.path.exists(visual_folder+image_name):
-                cv2.imwrite(visual_folder+image_name,img)
+            if not os.path.exists(visual_folder): 
+                os.makedirs(visual_folder, exist_ok = True)
+
+            assert os.path.exists(file_path),file_path
+            
+            cate=timu_data['Path'].split('/')[1]
+            image_name=str(pic_id)+'.jpg'
+            label_dict=json.loads(row['标注答案'])
+            if len(label_dict.keys()):
+                bboxes=label_dict['objects']
+                img = cv2.imread(file_path)
+                imgy,imgx=img.shape[:2]
+                
+                if len(bboxes):
+                    for bbox in bboxes:
+                        box_num+=1
+                        x_list=[bbox['data'][i]['x'] for i in range(0,4)]
+                        y_list=[bbox['data'][i]['y'] for i in range(0,4)]
+                        topleft=[int(max(min(x_list),0)),int(max(min(y_list),0))]
+                        bottomright=[int(min(max(x_list),imgx-1)),int(min(max(y_list),imgy-1))]
+                        cv2.rectangle(img, tuple(topleft), tuple(bottomright),(0,255,0), 2, 4)
+                        center_x=int((topleft[0]+bottomright[0])/2)
+                        center_y=int((topleft[1]+bottomright[1])/2)
+                        if len(bbox['tags']):
+                            if 'value'in bbox['tags'][0].keys():
+                                value=bbox['tags'][0]['value']+[cate]
+                                class_name=str(value)
+
+                                img=cv2ImgAddText(img, class_name , int(topleft[0]),center_y,(0,255,255),20)
+                            #img=cv2ImgAddText(img,str(pic_id),10,10,(0,0,0),30)
+                #ONLY plot the image if it is labeled.
+                if not os.path.exists(visual_folder+image_name):
+                    cv2.imwrite(visual_folder+image_name,img)
+                else:f.write(str(pic_id)+'\n')
+
 
     #return df_store
 if __name__ == "__main__":
