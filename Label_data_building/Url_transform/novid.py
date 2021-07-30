e2a13f99ecd80f0f39961dddeeb64812ee222374
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-30 14:16:32 +0800

    Update novid.py

--- Label_data_building/Url_transform/novid.py
+++ Label_data_building/Url_transform/novid.py
@@ -1,7 +1,7 @@
 import os
-base='D:\WWF_Det\WWF_Data\Raw_Data/rest-all'
+base='D:\WWF_Det\WWF_Data\Raw_Data/rest-p2'
 cp_base=base+'-novideo/'
-#os.system('cp -R '+base +' '+cp_base)
+os.system('cp -R '+base +' '+cp_base)
 folder_list=os.listdir(cp_base)
 print(cp_base)
 for i in folder_list:
