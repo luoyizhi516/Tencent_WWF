17208105220cee5807530bc7cc4f137a5a9018fb
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-07-31 13:52:01 +0800

    update

--- Pos_data_building/check-code/interactive_check.py
+++ Pos_data_building/check-code/interactive_check.py
@@ -6,16 +6,17 @@ def main():
     base,txt_path,start,end=opt.img_dir,opt.text_dir,opt.start,opt.end
     img_list=os.listdir(base)
     
-    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\xuebao-120-all/check3300-all.txt'
+    txt_path=r'D:\WWF_Det\WWF_Det\Drop_txt\top14-part7/check3000-3100.txt'
     with open(txt_path, 'w') as f:
 
-        for i in img_list[3300:]:
+        for i in img_list[3000:3100]:
             img_dir=base+i
             print(img_dir)
             
             img=cv2.imread(img_dir)
             win_name=img_dir
             cv2.namedWindow(win_name,0)
+            
             cv2.resizeWindow(win_name, 1024, 720)
             cv2.imshow(win_name,img)
             k=cv2.waitKey(0)
@@ -33,7 +34,7 @@ def main():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img_dir', type=str, default=r'D:\WWF_Det\WWF_Data\Pos_Data\xuebao-120-all\allset\visualizations/', help='dataset_store_dir')
+    parser.add_argument('--img_dir', type=str, default=r'D:\WWF_Det\WWF_Data\Pos_Data\top14-part7\allset\visualizations/', help='dataset_store_dir')
     parser.add_argument('--text_dir', type=str, default='check.txt', help='result')
     parser.add_argument('--start', type=int, default='0', help='start_img')
     parser.add_argument('--end', type=int, default='3210', help='start_img')
