fe7c0ab4993f29206b12d6063f84ae7082a5c489
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-08-06 18:16:53 +0800

    Update select_vid.py

--- Final_data_building/video_based_split/select_vid.py
+++ Final_data_building/video_based_split/select_vid.py
@@ -13,69 +13,40 @@ import pyfastcopy
 import json,sklearn
 from sklearn.model_selection import train_test_split
 
+pos_data_base='D:/WWF_Det/WWF_Data/Pos_Data/'
+raw_data_base='D:/WWF_Det/WWF_Data/Raw_Data/'
+
+annotation_base='D:/WWF_Det/WWF_Det/Raw_annoations/'
+
+Final_data_base='D:/WWF_Det/WWF_Data/Final_Data/rest-vid-clean/'
+try:
+    os.makedirs(Final_data_base)
+except:
+    pass
+
+combine_data_list=['rest-part1','rest-part2']
 
 
-def main():
-    pos_data_base='D:/WWF_Det/WWF_Data/Pos_Data/'
-    raw_data_base='D:/WWF_Det/WWF_Data/Raw_Data/top14-p1-p2-p3-merged/'
-    annotation_base='D:/WWF_Det/WWF_Det/Raw_annoations/'
-    vid_split_path='D:/WWF_Det/WWF_Det/Final_data_stat/top14-p123/video_split.csv'
-    split_file='D:/WWF_Det/WWF_Det/Final_data_stat/top14-p123/img_split2.csv'
-    Final_data_base='D:/WWF_Det/WWF_Data/Final_Data/top14-p123/'
-    train_img_dir,train_txt_dir=Final_data_base+'/train/images/',Final_data_base+'/train/labels/'
-    test_img_dir,test_txt_dir=Final_data_base+'/val/images/',Final_data_base+'/val/labels/'
-    if not os.path.exists(train_img_dir):os.makedirs(train_img_dir)
-    if not os.path.exists(train_txt_dir):os.makedirs(train_txt_dir)
-    if not os.path.exists(test_img_dir):os.makedirs(test_img_dir)
-    if not os.path.exists(test_txt_dir):os.makedirs(test_txt_dir)
-    data_dict={
-        'img_path':[],
-        'video_path':[],
-        'cate':[],
-        'modality':[],
-        'label':[]
-    }
-    combine_data_list=['top14-part1','top14-part2','top14-part3']
-    df_vid=pd.read_csv(vid_split_path)
-    for dataset in combine_data_list:
-        valuableset_dir=pos_data_base+dataset+'/valuableset/images/'
-        valuableset_txt_dir=pos_data_base+dataset+'/valuableset/labels/'
-        annotation_dir=annotation_base+dataset+'.csv'
-        df=pd.read_csv(annotation_dir)
-        pic_id_list=[i.replace('.jpg','',1) for i in os.listdir(valuableset_dir)]
-        
-        for pic_id in tqdm(pic_id_list):
+for dataset in combine_data_list:
+    source_vid_path_all=[]
+    target_vid_path_all=[]
+    valuableset_dir=pos_data_base+dataset+'/allset/visualizations/'
+    source_base=raw_data_base+dataset+'/'
+    source_base=source_base.replace('part','p')
+    annotation_dir=annotation_base+dataset+'.csv'
+    df=pd.read_csv(annotation_dir)
+    pic_id_list=[i.replace('.jpg','',1) for i in os.listdir(valuableset_dir)]
+    for pic_id in tqdm(pic_id_list):
 
             pic_df=df.loc[df['题目ID']== int(pic_id)]
             timu_str=pic_df['题目数据'].values[0]
             timu_data=json.loads(timu_str)
             video_path_list=timu_data['video_path'].split('/')
             vid_path=video_path_list[-3]+'/'+video_path_list[-2]+'/'+video_path_list[-1]
-            pic_path=str(valuableset_dir+pic_id+'.jpg').replace('D:/WWF_Det/WWF_Data/Pos_Data/','')
-            vid_df=df_vid.loc[df_vid['video_path']==raw_data_base+vid_path]
-            label=vid_df['label'].values[0]
-            modality=vid_df['modality'].values[0]
-            cate=vid_df['cate'].values[0]
-            if label=='train':
-                shutil.copyfile(valuableset_dir+pic_id+'.jpg',train_img_dir+pic_id+'.jpg')
-                shutil.copyfile(valuableset_txt_dir+pic_id+'.txt',train_txt_dir+pic_id+'.txt')
-            elif label=='test':
-                shutil.copyfile(valuableset_dir+pic_id+'.jpg',test_img_dir+pic_id+'.jpg')
-                shutil.copyfile(valuableset_txt_dir+pic_id+'.txt',test_txt_dir+pic_id+'.txt')
-            data_dict['img_path'].append(pic_path)
-            data_dict['video_path'].append(vid_path)
-            data_dict['cate'].append(cate)
-            data_dict['modality'].append(modality)
-            data_dict['label'].append(label)
-            
-    df_store=pd.DataFrame(data_dict)
-    df_store.to_csv(split_file,index=False)
-    print(df_store)
-        #print(df_vid)
-        #break
+            source_vid_path_all.append(vid_path)
+            target_vid_path_all.append(video_path_list[-3]+'-'+video_path_list[-1])
+    source_vid_path_all=np.unique(np.array(source_vid_path_all)).tolist()
+    target_vid_path_all=np.unique(np.array(target_vid_path_all)).tolist()
 
-            
-
-if __name__ == "__main__":
-    
-    main()
+    for source_vid,target_vid in zip(source_vid_path_all,target_vid_path_all):
+            shutil.copyfile(source_base+source_vid,Final_data_base+target_vid)   
