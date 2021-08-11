aff5594e09faae24c405f4902329f1160bcb2096
Jingyuanchen1423 <jingyuanchen1423@gmail.com>
2021-08-12 07:43:01 +0800

    update

--- Final_data_building/video_based_split/count_vid_per_class.py
+++ Final_data_building/video_based_split/count_vid_per_class.py
@@ -5,7 +5,7 @@ from collections import Counter
 import pandas as pd
 import shutil
 import numpy as np
-import cv2
+
 import random
 from tqdm import tqdm
 import pyfastcopy
@@ -22,5 +22,13 @@ def cate_replacement(cate_name):
 
 Final_vid_base='D:/WWF_Det/WWF_Data/Final_Data/rest-vid-clean/'
 vid_list=os.listdir(Final_vid_base)
-cate_list=[cate_replacement(i.split('-')[0]) for i in vid_list]
-print(cate_list)
+cate_list=dict(Counter([cate_replacement(i.split('-')[0]) for i in vid_list]))
+new_dict={'cate':[],'video_num':[]}
+for i in cate_list:
+    new_dict['cate'].append(i)
+    new_dict['video_num'].append(cate_list[i])
+df=pd.DataFrame(new_dict)
+df=df.sort_values(by="video_num" , ascending=False)
+df=df.reset_index().drop(['index'], axis=1)
+df.to_csv(r'D:\WWF_Det\WWF_Det\Final_data_stat\rest-cate\vid_stat.csv',index=False)
+print(df)
